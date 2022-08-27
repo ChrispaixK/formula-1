196802027a1a4bab96400c427d2d2fa6b0f0b2c1
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-08-27 12:22:32 +0200

    Style nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import '../assets/css/Navbar.css';
 
 const Navbar = () => {
   return (
-    <>
-      <p>Navbar here</p>
-    </>
+    <div className="navbar">
+      <div className="nav-left">
+        <p>
+          <i className="fa-solid fa-angle-left" />
+          2018
+        </p>
+      </div>
+      <div className="nav-center">
+        <p>Home page</p>
+      </div>
+      <div className="nav-right">
+        <p>
+          <i className="fa-solid fa-microphone" />
+          <i className="fa-solid fa-gear" />
+        </p>
+      </div>
+    </div>
   );
 };
 
